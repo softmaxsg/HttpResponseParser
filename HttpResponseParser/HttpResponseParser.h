c08f19c310f08735146bcab3b6b82764e35532bc
Vitaly Chupryk <vitaly@softmaxsg.com>
2019-01-30 19:27:53 +0100

    Initial commit

---
+++ HttpResponseParser/HttpResponseParser.h
@@ -0,0 +1,11 @@
+//
+//  Copyright © 2019 Vitaly Chupryk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HttpResponseParser.
+FOUNDATION_EXPORT double HttpResponseParserVersionNumber;
+
+//! Project version string for HttpResponseParser.
+FOUNDATION_EXPORT const unsigned char HttpResponseParserVersionString[];
